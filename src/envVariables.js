30756c2c81cadf529df14339a5ccf0f2937930b0
Anshu <me.anshu7@gmail.com>
2020-06-02 19:04:03 +0530

    first commit

--- src/envVariables.js
+++ src/envVariables.js
@@ -7,16 +7,12 @@ const envVariables = {
   DB_SSL: process.env.DB_SSL || true,
   DB_PORT: process.env.DB_PORT || 5432,
   DB_MAX_POOL_SIZE: process.env.DB_MAX_POOL_SIZE || "5",
+  
   //server configurations
   SERVER_PORT: process.env.SERVER_PORT || "8080",
   PORT: 8080,
   BODY_LIMIT: "100kb",
   CROS_HEADERS: ["Link"],
 
-  // DO Configuration
-  DO_ACCESS_KEY: process.env.DO_ACCESS_KEY || '5EB2UWR4QRF62V2P2CYK',
-  DO_SECRET_ACCESS_KEY: process.env.DO_SECRET_ACCESS_KEY || '7Aw/z7eddtpmUrodsCuz94SJveaxnqRqm9S0r3uuPjU',
-  DO_BUCKET_NAME: process.env.DO_BUCKET_NAME || 'gwl-product-file-storage', // gwl-product-file-storage // gwl-bucket
-  DO_REGION: process.env.DO_REGION || 'ams3',
 };
 export default envVariables;
