f15da06f558049bf2065f776b26d5493d22b425d
nea <neha.kumari@goodworklabs.com>
2020-06-08 14:05:32 +0530

    2nd commit

--- src/api/index.js
+++ src/api/index.js
@@ -12,25 +12,7 @@ import envVariables from '../envVariables';
 export default ({ config, db }) => {
   let api = Router();
 
-  /* api.post("/upload_file", (req, res) => {
-
-    const form = formidable({ multiples: true });
-    let responseArr = [];
-
-    form.parse(req, asyncHandler(async (err, fields, files) => {
-      if (err) {
-        next(err);
-        return;
-      };
-
-      // console.log('#fields', fields);
-      // console.log('#files', files);
-
-      responseArr = await fileUpload(fields, files);
-
-    }));
-
-  }); */
+ 
 
   api.post("/upload_file2", asyncHandler(async (req, res) => {
 
@@ -46,17 +28,14 @@ export default ({ config, db }) => {
 
         const { key } = req.body || 'ams-docs';
 
-        // console.log('#fields', fields);
-        // console.log('#files', files);
+       
 
         for (const file in files) {
           let fileUploadAW = await fileUpload(key, files[file]);
           responseArr.push(fileUploadAW);
         };
 
-        // console.log(fileUploadAW);
-
-        // responseArr = await fileUpload(fields, files);
+       
         res.status(200).json({ "uploadResponse": responseArr });
       });
     } catch (e) {
